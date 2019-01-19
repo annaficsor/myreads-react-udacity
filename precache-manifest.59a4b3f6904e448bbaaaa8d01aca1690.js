44668e8c02a75f1a968d096d49e3eb5065e30f0f
Anna Ficsor <ficsor.anna@gmail.com>
2019-01-19 12:02:32 +0100

    Updates

---
+++ precache-manifest.59a4b3f6904e448bbaaaa8d01aca1690.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/myreads-react-udacity/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "c4f0e65f02e850dc0eaf71322577b5de",
+    "url": "/myreads-react-udacity/static/media/arrow-drop-down-black.c4f0e65f.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/myreads-react-udacity/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/myreads-react-udacity/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "124ea636ca2d914bde6a",
+    "url": "/myreads-react-udacity/static/js/runtime~main.124ea636.js"
+  },
+  {
+    "revision": "61cada2566acf7a6a2d0",
+    "url": "/myreads-react-udacity/static/js/main.61cada25.chunk.js"
+  },
+  {
+    "revision": "8ccf883cc34ad6d67a2c",
+    "url": "/myreads-react-udacity/static/js/1.8ccf883c.chunk.js"
+  },
+  {
+    "revision": "61cada2566acf7a6a2d0",
+    "url": "/myreads-react-udacity/static/css/main.768080bb.chunk.css"
+  },
+  {
+    "revision": "20c4aa97717f6e6b1a69ff494c75c098",
+    "url": "/myreads-react-udacity/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,34 @@
-"use strict";var precacheConfig=[["/myreads-react-udacity/index.html","b64af9a1d7237f75e53f46279dd22c55"],["/myreads-react-udacity/static/css/main.1e984a0f.css","04e2a224d4a3d7051224cc2c07c0cd55"],["/myreads-react-udacity/static/js/main.229161a8.js","0661bbc4c7c6a1e6e5759b5744eab399"],["/myreads-react-udacity/static/media/add.75d390e5.svg","75d390e5eabc3254418e950df631389b"],["/myreads-react-udacity/static/media/arrow-back.20e8847d.svg","20e8847d1eb7759c19b0e30beb8849cf"],["/myreads-react-udacity/static/media/arrow-drop-down-black.c4f0e65f.svg","c4f0e65f02e850dc0eaf71322577b5de"],["/myreads-react-udacity/static/media/arrow-drop-down.9d4e3f36.svg","9d4e3f366f039249695de85a30a02b95"]],cacheName="sw-precache-v3-sw-precache-webpack-plugin-"+(self.registration?self.registration.scope:""),ignoreUrlParametersMatching=[/^utm_/],addDirectoryIndex=function(e,t){var r=new URL(e);return"/"===r.pathname.slice(-1)&&(r.pathname+=t),r.toString()},cleanResponse=function(t){return t.redirected?("body"in t?Promise.resolve(t.body):t.blob()).then(function(e){return new Response(e,{headers:t.headers,status:t.status,statusText:t.statusText})}):Promise.resolve(t)},createCacheKey=function(e,t,r,n){var a=new URL(e);return n&&a.pathname.match(n)||(a.search+=(a.search?"&":"")+encodeURIComponent(t)+"="+encodeURIComponent(r)),a.toString()},isPathWhitelisted=function(e,t){if(0===e.length)return!0;var r=new URL(t).pathname;return e.some(function(e){return r.match(e)})},stripIgnoredUrlParameters=function(e,r){var t=new URL(e);return t.hash="",t.search=t.search.slice(1).split("&").map(function(e){return e.split("=")}).filter(function(t){return r.every(function(e){return!e.test(t[0])})}).map(function(e){return e.join("=")}).join("&"),t.toString()},hashParamName="_sw-precache",urlsToCacheKeys=new Map(precacheConfig.map(function(e){var t=e[0],r=e[1],n=new URL(t,self.location),a=createCacheKey(n,hashParamName,r,/\.\w{8}\./);return[n.toString(),a]}));function setOfCachedUrls(e){return e.keys().then(function(e){return e.map(function(e){return e.url})}).then(function(e){return new Set(e)})}self.addEventListener("install",function(e){e.waitUntil(caches.open(cacheName).then(function(n){return setOfCachedUrls(n).then(function(r){return Promise.all(Array.from(urlsToCacheKeys.values()).map(function(t){if(!r.has(t)){var e=new Request(t,{credentials:"same-origin"});return fetch(e).then(function(e){if(!e.ok)throw new Error("Request for "+t+" returned a response with status "+e.status);return cleanResponse(e).then(function(e){return n.put(t,e)})})}}))})}).then(function(){return self.skipWaiting()}))}),self.addEventListener("activate",function(e){var r=new Set(urlsToCacheKeys.values());e.waitUntil(caches.open(cacheName).then(function(t){return t.keys().then(function(e){return Promise.all(e.map(function(e){if(!r.has(e.url))return t.delete(e)}))})}).then(function(){return self.clients.claim()}))}),self.addEventListener("fetch",function(t){if("GET"===t.request.method){var e,r=stripIgnoredUrlParameters(t.request.url,ignoreUrlParametersMatching),n="index.html";(e=urlsToCacheKeys.has(r))||(r=addDirectoryIndex(r,n),e=urlsToCacheKeys.has(r));var a="/myreads-react-udacity/index.html";!e&&"navigate"===t.request.mode&&isPathWhitelisted(["^(?!\\/__).*"],t.request.url)&&(r=new URL(a,self.location).toString(),e=urlsToCacheKeys.has(r)),e&&t.respondWith(caches.open(cacheName).then(function(e){return e.match(urlsToCacheKeys.get(r)).then(function(e){if(e)return e;throw Error("The cached response that was expected is missing.")})}).catch(function(e){return console.warn('Couldn\'t serve response for "%s" from cache: %O',t.request.url,e),fetch(t.request)}))}});
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/myreads-react-udacity/precache-manifest.59a4b3f6904e448bbaaaa8d01aca1690.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/myreads-react-udacity/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
